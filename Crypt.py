a0acc0493a7b00ba688998545d0e2fbaca4a99e2
Hunter Krasnicki <70709871+HK-CSE@users.noreply.github.com>
2023-12-29 05:57:13 -0500

    Update Crypt.py
    Improved Comments

--- Crypt.py
+++ Crypt.py
@@ -4,23 +4,26 @@ def LSFRKeyGen(iv, dataLen) -> bytes:
     feedback = 0x87654321
     key = []
         
-    #For each byte, cycle the Linear Shift Feedback Register 8 times, extract the lowest byte, append to key bytes    
+    # For each byte in data, cycle the Linear Shift Feedback Register 8 times, extract the lowest byte, append to key bytes    
     for i in range(0, dataLen):
         
         for j in range(0, 8):
-                
+            
+            # Check the lowest significant bit, if it's 1, logical right shift the current register state by 1 
+            # Then XOR with the preset feedback
             LSB = bin(state)[-1]
             if (LSB == '1'):
                 state = (state >> 1) ^ feedback
+            # Otherwise, if the lowest signficant bit is 0, logical right shift the current register state by 1
             else:
                 state = state >> 1
-                
+        # Extract the lower 8 bits (1 byte) from the current register state, convert to int of base 2, append to current key        
         keyByte = int(bin(state)[-8:], 2)
         key.append(keyByte)
             
     return key
 
-# Takes data and of equal length as bytes and performs a bitwise XOR operation on each byte
+# Takes in data and key of equal length as bytes, performs a bitwise XOR operation between data and key
 def XORData(data: bytes, key: bytes, dataLen: int) -> bytes:
     xor = []
     
@@ -39,6 +42,7 @@ def Crypt(data: bytes, initialValue: int) -> bytes:
     key = LSFRKeyGen(initialValue, dataLen)
     EncryptBytes = XORData(data, key, dataLen)
     
+    # Format byte output as a bytestring, string output as the character representation of each byte
     outBytes = ''.join(['\\x{:02x}'.format(x) for x in EncryptBytes])
     outStr = ''.join(chr(byte) for byte in EncryptBytes)
     
@@ -48,11 +52,11 @@ def Crypt(data: bytes, initialValue: int) -> bytes:
     
 if __name__ == '__main__':
     
-    #Test Cases
+    # Test Cases
     test1 = b'apple'                
-    test1b = b'\xcd\x01\xef\xd7\x30' #'apple' in bytes
+    test1b = b'\xcd\x01\xef\xd7\x30' # 'apple' in bytes
     test2 = b'Thisisateststring'
-    test2b = b'\xf8\x19\xf6\xc8\x3c\x40\x43\xad\xaf\xb4\xeb\x8f\xc4\x0e\x98\x08\x86' #'Thisisateststring' in bytes            
+    test2b = b'\xf8\x19\xf6\xc8\x3c\x40\x43\xad\xaf\xb4\xeb\x8f\xc4\x0e\x98\x08\x86' # 'Thisisateststring' in bytes            
         
     print(Crypt(test1, 0x12345678))
     print(Crypt(test1b, 0x12345678))
