f78e9a727d907bfed579cb7e07632b7963fce005
Hunter Krasnicki <70709871+HK-CSE@users.noreply.github.com>
2023-12-29 05:29:50 -0500

    Update Crypt.py
    Added comments

--- Crypt.py
+++ Crypt.py
@@ -1,10 +1,11 @@
-def LSFRKeyGen(iv, dataLen) -> list:
+# Takes initial value supplied to Crypt, produces a key as a bytes object
+def LSFRKeyGen(iv, dataLen) -> bytes:
     state = 0 or iv
     feedback = 0x87654321
     key = []
         
+    #For each byte, cycle the Linear Shift Feedback Register 8 times, extract the lowest byte, append to key bytes    
     for i in range(0, dataLen):
-        #print(f'Current key is {"".join(chr(byte) for byte in key)}')
         
         for j in range(0, 8):
                 
@@ -18,27 +19,25 @@ def LSFRKeyGen(iv, dataLen) -> list:
         key.append(keyByte)
             
     return key
-            
-def XORData(data: bytes, key: bytes, dataLen: int) -> list:
+
+# Takes data and of equal length as bytes and performs a bitwise XOR operation on each byte
+def XORData(data: bytes, key: bytes, dataLen: int) -> bytes:
     xor = []
     
     for i in range(0, dataLen):
 
         xorByte = data[i] ^ key[i] 
-        #print(f'In XORDATA: data[i] = {ord(data[i])}, key[i] = {key[i]}')
         xor.append(xorByte)
         
     return xor
 
 
+# Takes in data as a bytes object & initial value as an integer, performs LSRF based encryption/decryption on data
 def Crypt(data: bytes, initialValue: int) -> bytes:
-    #data = data.decode('unicode-escape')
-    #print(data)
-    #print(f'IV is {iv}')
+    
     dataLen = len(data)
     key = LSFRKeyGen(initialValue, dataLen)
     EncryptBytes = XORData(data, key, dataLen)
-    print(EncryptBytes)
     
     outBytes = ''.join(['\\x{:02x}'.format(x) for x in EncryptBytes])
     outStr = ''.join(chr(byte) for byte in EncryptBytes)
@@ -48,13 +47,14 @@ def Crypt(data: bytes, initialValue: int) -> bytes:
     
     
 if __name__ == '__main__':
-                    
+    
+    #Test Cases
     test1 = b'apple'                
-    test2 = b'\xcd\x01\xef\xd7\x30'
-    test3 = b'Thisisteststring'            
+    test1b = b'\xcd\x01\xef\xd7\x30' #'apple' in bytes
+    test2 = b'Thisisateststring'
+    test2b = b'\xf8\x19\xf6\xc8\x3c\x40\x43\xad\xaf\xb4\xeb\x8f\xc4\x0e\x98\x08\x86' #'Thisisateststring' in bytes            
         
     print(Crypt(test1, 0x12345678))
+    print(Crypt(test1b, 0x12345678))
     print(Crypt(test2, 0x12345678))
-    print(Crypt(test3, 0x12345678))
-    print(Crypt(b'\xf8\x19\xf6\xc8\x3c\x40\x56\xbc\xb9\xb3\xec\x88\xc2\x15\x9f\x01', 0x12345678))
-    #print(Crypt(input('Enter your data: ').encode('utf-8'), 0x12345678))
+    print(Crypt(test2b, 0x12345678))
